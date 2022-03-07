b68e258a70a9be9506ff5ca0dff5900dc3d706b6
Raman <sahiraman7@gmail.com>
2022-03-07 13:29:12 +0530

    user logging - 2

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import posts from './posts';
 import { combineReducers } from 'redux';
-
+import auth from './auth';
 export default combineReducers({
   posts,
+  auth,
 });
 
 //combineReducers({ posts, users })
